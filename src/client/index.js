6b3d4772127aa44cd93b0a71c9aacca2425de2b1
hreidar17 <hreidar17@ru.is>
2018-10-23 18:35:37 +0000

    Website initialised

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+function component() {
+    let element = document.createElement('div');
+    element.innerHTML = '<br><strong>Hello human...<br>Script is now connected</strong>';
+    return element;
+}
+
+document.body.appendChild(component());
+
