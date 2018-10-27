f4780a8668ada0f0ebf51201f84c71c3378c5369
Haddib <hb150794@gmail.com>
2018-10-27 17:13:56 +0000

    MIKLAR BREYTINGAR!!! Fyrst ekkert var að virka hjá okkur í sambandi við að setja þetta upp á vefsíðu verður þetta núna console leikur

--- src/client/index.js
+++ src/client/index.js
@@ -1,48 +1,34 @@
-import './../styles/style.css';
-
 var originalBoard;
 var currentPlayer = 'X';
 //const secondPlayer = 'O';
-const cells = document.querySelectorAll('.cell');
 
-
-startGame();
 
 function startGame(){
-    //document.querySelector(".endgame").style.display = "none";
-    originalBoard = Array.from(Array(9).keys());
-    for (var i = 0; i < cells.length; i++){
-        cells[i].innerText = '';
-        cells[i].addEventListener('click', turnClick, false);
-    }
+
+}
+
+function startGame(){
+
 }
 
 function resetButton() {
-    let element = document.createElement('div');
-    //element.innerHTML = '<br><strong>Hello human...<br>Script is now connected</strong>';
-    element.innerHTML = "<button id=\"resButton\">Reset Game</button>";
-    element.addEventListener('click', startGame, false);
-    return element;
+
 }
 
-document.body.appendChild(resetButton());
 
 function turnClick(square){
-    console.log(square.target.id);
-    if(square.target.innerText == ''){
-        console.log("input player " +currentPlayer);
-        document.getElementById(square.target.id).innerText = currentPlayer;
-        switchPlayer();
-    }
+
 }
 
-function switchPlayer(){
+function switchPlayer(currentPlayer){
     if(currentPlayer == 'X'){
-        currentPlayer = 'O';
+        return 'O';
     }
     else{
-        currentPlayer = 'X';
+        return 'X';
     }
 }
 
-
+module.exports = {
+  switchPlayer
+}
