fef29b8a26f040430e54c372e4b6b7fdc8e544c8
magnustorfi <magnustorfi@gmail.com>
2018-10-28 16:30:54 +0000

    newgame function komin

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,15 @@
 const game = require('./../logic/gameLogic');
 var prompt = require('prompt');
-var clear = require('clear');
 var board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 var currentPlayer = 'X';
 var Xscore = 0;
 var Oscore = 0;
 
 function startGame(){
-  game.clearBoard(board);
+  prompt.start();
+  board = game.clearBoard(board);
+  currentPlayer = 'X';
+  //game.clearBoard(board);
   printBoard();
   playerMove(currentPlayer);
 
@@ -15,10 +17,14 @@ function startGame(){
 
 function playerMove(player){
   console.log("Player turn is " +player);
-  prompt.start();
+  
   prompt.get(['position'], function(err, result)
   {
-    var input = game.verifyInput(result.position);
+    //var str = result.position;
+    var input = game.verifyInput(result.position.charAt(0));
+
+
+
     if(!isNaN(input))
     {
       if(Number.isInteger(board[input]))
@@ -28,12 +34,12 @@ function playerMove(player){
         if(game.checkForWin(player, board))
         {
           console.log(player +' WINS!');
-          return;
+          return newGame();          
         }
         if(game.checkForTie(board))
         {
           console.log('The game is tied');
-          return;
+          return newGame();
         }
         currentPlayer = game.switchPlayer(player);
         playerMove(currentPlayer);
@@ -54,8 +60,24 @@ function playerMove(player){
   });
 }
 
+function newGame(){
+  var playerInput = '';
+  console.log('Do you want to play again? y/n');
+  prompt.get(['newGame'], function (err, result) {
+  console.log(' ' + result.newGame);
+    
+    playerInput = result.newGame.charAt(0);
+    if(playerInput == 'y' || playerInput == 'Y'){
+      startGame(); 
+    }
+    else{
+      process.exit(1);
+
+    }
+  });
+}
+
 function printBoard() {
-  clear();
     console.log('\n\n\n' +
         ' ' + board[0] + ' | ' + board[1] + ' | ' + board[2] + '\n' +
         ' ---------\n' +
